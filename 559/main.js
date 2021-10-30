b2e4c6ae4ec25d070dde2eaaa87e80fcdac4cb4d
Sangmin <besforyou2@gmail.com>
2021-10-30 23:10:17 +0900

    Maximum Depth of N-ary Tree

---
+++ 559/main.js
@@ -0,0 +1,15 @@
+
+var maxDepth = function(root) {
+    
+    if ( root == null )
+        return 0;
+    
+    let max = 0;
+    for (let child of root.children) {
+        max = Math.max(max, maxDepth(child));
+    }
+    
+    return max + 1;
+};
+
+
