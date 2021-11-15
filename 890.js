4ee4ea1bb47efa029928142b17d47138eba51e32
Sangmin <besforyou2@gmail.com>
2021-11-15 22:15:46 +0900

    890. Find and Replace Pattern

--- 890.js
+++ 890.js
@@ -5,15 +5,15 @@ var findAndReplacePattern = function(words, pattern) {
     
     for (let i = 0 ; i < pattern.length ; i++) {
         let letter = pattern.charAt(i);
-        if (!patternMap.has(letter)) patternMap.set(letter, 1);
+        if (!patternMap.has(letter)) {
+            patternMap.set(letter, patternMap.size);
+            patternValue.push(patternMap.size);
+        } 
         else if (patternMap.has(letter)) {
             let num = patternMap.get(letter);
-            patternMap.set(letter, ++num);
+            patternValue.push(num);
         }
     }
-    
-    for (const value of patternMap.values()) patternValue.push(value);
-    
     
     for (let i = 0 ; i < words.length ; i++) {
         let word = words[i];
@@ -21,19 +21,16 @@ var findAndReplacePattern = function(words, pattern) {
         let wordsValue = [];
         for (let i = 0 ; i < word.length ; i++) {
             let letter = word.charAt(i);
-            if (!map.has(letter)) map.set(letter, 1);
+            if (!map.has(letter)) { 
+                map.set(letter, map.size);
+                wordsValue.push(map.size);
+            }
             else if (map.has(letter)) {
                 let num = map.get(letter);
-                map.set(letter, ++num);
+                wordsValue.push(num);
             }
         }
-        
-        for (const value of map.values()) wordsValue.push(value);
-        
-        if( JSON.stringify(patternValue) == JSON.stringify(wordsValue)) {
-            ans.push(word);
-        } 
-        
+        if( JSON.stringify(patternValue) == JSON.stringify(wordsValue)) ans.push(word);
     }
     return ans;
 };
