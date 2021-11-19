17ad1c14e4bae5e41394a82b97d0f1e7b2be3b28
Sangmin <besforyou2@gmail.com>
2021-11-19 19:54:49 +0900

    442. Find All Duplicates in an Array

--- 442.js
+++ 442.js
@@ -1,3 +1,5 @@
+// version 1
+
 var findDuplicates = function(nums) {
     
     const mySet = new Set();
@@ -12,3 +14,22 @@ var findDuplicates = function(nums) {
     
     return ans;
 };
+
+// better version 2
+
+var findDuplicates = function(nums) {
+
+    let len = nums.length
+    let arr = new Array(len+1).fill(0)
+    let ans = [];
+
+    for (let i = 0 ; i < len ; i++) {
+        let num = nums[i];
+        if (arr[num] == 0) arr[num] = 1;
+        else if (arr[num] == 1) ans.push(num);
+    }
+
+    return ans;
+}
+
+
