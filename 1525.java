ea36f8c54d53c1f3e1bdfbd1bb61b42686b7ec2a
Sangmin <besforyou2@gmail.com>
2021-11-20 11:29:41 +0900

    1525. Number of Good Ways to Split a String

--- 1525.java
+++ 1525.java
@@ -1,3 +1,4 @@
+// hashmap version
 class Solution {
         public int numSplits(String s) {
 
@@ -24,3 +25,20 @@ class Solution {
             return count;
         }
     }
+
+// faster solution : two-point
+class Solution {
+	public int numSplits(String s) {
+		int rc[] = new int[26], lc[] = new int[26], l = 0, r = 0, res = 0;
+		for ( char c : s.toCharArray() ) if ( rc[c - 'a']++ == 0 ) r++;
+
+		for ( char c : s.toCharArray() ) {
+			if (lc[c - 'a']++ == 0 ) l++;
+			if (--rc[c - 'a'] == 0 ) r--;
+			if ( l == r ) res++;
+		}
+
+		return res;
+	}
+}
+
