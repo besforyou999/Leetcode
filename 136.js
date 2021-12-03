a8fc1c92823eaa818650da1416b9ee3fbd0f0195
Sangmin <besforyou2@gmail.com>
2021-12-03 22:53:46 +0900

    136. Single Number

--- 136.js
+++ 136.js
@@ -1,3 +1,14 @@
+// Time : O(N), space : O(1)
+var singleNumber = function(nums) {
+
+    let num = nums[0];
+
+    for (let i = 1 ; i < nums.length ; i++) num ^= nums[i];
+
+    return num;
+};
+
+// Time : O(N) , space : O(N)
 var singleNumber = function(nums) {
     
     const map = {};
