3d9003a0c053e3d008e6f9cdadaba100d9d10dff
Sangmin <besforyou2@gmail.com>
2021-12-05 12:18:48 +0900

    961. N-Repeated Element in Size 2N Array

--- 961.js
+++ 961.js
@@ -1,3 +1,18 @@
+// array
+
+var repeatedNTimes = function(nums) {
+    
+    let n = nums.length / 2;
+    
+    let arr = new Array(10001).fill(0);
+    
+    for (const num of nums) {
+        arr[num] += 1;
+        if (arr[num] == n) return num;
+    }
+};
+
+// map
 var repeatedNTimes = function(nums) {
     
     const map = {};
