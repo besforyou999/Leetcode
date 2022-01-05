55b957c0ad8d440ec834691b69b4cb8267a299ee
Sangmin <besforyou2@gmail.com>
2022-01-05 23:56:47 +0900

    110. Balanced Binary Tree

--- 110/main.js
+++ 110/main.js
@@ -1,21 +1,17 @@
-var isBalanced = function(root) {    
-    let ans = true;    
+var isBalanced = function(root) {
+    
+    var ans = true;
+    
     let dfs = function(root) {
-        
-        if(!root) return 0;
-        if(!ans) return 0;
-        
-        let leftTree = dfs(root.left);
-        let rightTree = dfs(root.right);
-        
-        if (Math.abs(leftTree - rightTree) > 1) ans = false;    
-        
-        return 1 + Math.max(leftTree, rightTree);
-    }     
-
+        if (!root) return 0;
+        if (!ans)  return 0;
+        let left  = dfs(root.left);
+        let right = dfs(root.right);
+        if (Math.abs(left - right) > 1) ans = false;
+        return 1 + Math.max(left, right);
+    }
+    
     dfs(root);
     
-    return ans;  
+    return ans;
 };
-
-
