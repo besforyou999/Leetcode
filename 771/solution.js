851707ec046dd330305f73124d4aeb03894cac0f
Sangmin <besforyou2@gmail.com>
2022-01-06 20:44:51 +0900

    771. Jewels and Stones

--- 771/solution.js
+++ 771/solution.js
@@ -33,3 +33,20 @@ var numJewelsInStones = function(jewels, stones) {
 
     return ans;
 };
+
+
+// best solution
+var numJewelsInStones = function(jewels, stones) {
+    
+    var obj = {};
+    var count = 0;
+    for (let i = 0; i < jewels.length ; i++)
+        obj[jewels[i]] = true;
+    
+    for (let i = 0 ; i < stones.length ; i++) {
+        if (obj[stones[i]])
+            count++;
+    }
+    
+    return count;
+};
