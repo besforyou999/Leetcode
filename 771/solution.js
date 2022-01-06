4bc2060f89d3b5e603ce988dda0b37903908803b
Sangmin <besforyou2@gmail.com>
2022-01-06 20:01:25 +0900

    771. Jewels and Stones

--- 771/solution.js
+++ 771/solution.js
@@ -13,3 +13,23 @@ var numJewelsInStones = function(jewels, stones) {
     
     return count;
 };
+
+
+// my solution using Map
+var numJewelsInStones = function(jewels, stones) {
+
+    var dic = new Map();
+    var ans = 0, letter;
+
+    for (let i = 0 ; i < jewels.length ; i++) {
+        letter = jewels[i];
+        if (!dic.has(letter)) dic.set(letter, 1);
+    }
+
+    for (let i = 0 ; i < stones.length ; i++) {
+        letter = stones[i];
+        if (dic.has(letter)) ans += 1;
+    }
+
+    return ans;
+};
