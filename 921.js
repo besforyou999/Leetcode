f05b2a8fde72be113ec9e1e477865e43aed671ff
Sangmin <besforyou2@gmail.com>
2022-04-26 14:46:54 +0900

    921. Minimum Add to Make Parentheses Valid ( Javascript )

---
+++ 921.js
@@ -0,0 +1,23 @@
+var minAddToMakeValid = function(s) {
+
+    const stack = new Array();
+
+    for ( let i = 0 ; i < s.length ; i++) {
+        let ch = s[i];
+        if ( ch == "(" ) {
+            stack.push(ch);
+        } 
+        else if ( stack.length != 0 ) {
+            let top = stack[stack.length - 1];
+            if ( top == "(" )
+                stack.pop();
+            else
+                stack.push(ch);
+        } 
+        else if ( stack.length == 0 ) {
+            stack.push(ch);
+        }        
+    }
+    return stack.length;
+};
+
