f4127e3813a8bb7d545a93ad23713266daa8f7f7
주상민 <besforyou2@gmail.com>
2023-10-04 12:10:18 +0900

    1043. Partiton Array for Maximum Sum (Python)

---
+++ algorithms/1043.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxSumAfterPartitioning(self, arr, k):
+        n = len(arr)
+        dp = [0] * n
+
+        # handle the first k indexes differently
+        for j in range(k): dp[j] = max(arr[:j+1]) * (j+1)
+
+        # we can get rid of index i by running i times
+        for j in range(k, n):
+            curr = []
+            for m in range(k):
+                curr.append(dp[j-m-1] + max(arr[(j-m):(j+1)]) * (m+1))
+            dp[j] = max(curr)
+    
+        return dp[-1]
+
